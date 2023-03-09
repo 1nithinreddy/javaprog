7cc9aa53a53d83e0f1a40c8a330c068255df0b7f
1nithinreddy <127389905+1nithinreddy@users.noreply.github.com>
2023-03-09 14:25:43 +0530

    programmes added

---
+++ double.java
@@ -0,0 +1,8 @@
+class double
+{
+public static void main(String[]args)
+{
+double number=-42.834787635809534320
+System.out.println(number);
+}
+}
---
+++ literals.java
@@ -0,0 +1,10 @@
+public class literals
+{
+public static void main(String[]args)
+{
+float a=101.230f;
+float b=0123.24f;
+System.out.println(a);
+System.out.println(b);
+}
+}
